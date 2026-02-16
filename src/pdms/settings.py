c864a5ea67c06ef175a97db864f640f3e430a143
slasherdrake <dkdrakekendall@gmail.com>
2026-02-16 13:04:08 -0700

    moved api

--- src/pdms/settings.py
+++ src/pdms/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'pdms.api',
     'corsheaders',
     'rest_framework',
 ]
