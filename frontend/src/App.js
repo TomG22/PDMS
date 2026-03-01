7a9c234b5ea80b7af300fe848fecf234ef90bec6
amyrawhxte <amyraawhite04@gmail.com>
2026-02-28 19:06:13 -0700

    Basic UI for frontend, added features for ADD, REMOVE, EDIT, VIEW

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
+import ProjectDashboard from "./pages/ProjectDashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/project-dash-test" element={<ProjectDashboard />} />
       </Routes>
     </BrowserRouter>
   );
