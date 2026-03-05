e1be5a1152508a308845c13af93aa43077811c4b
slasherdrake <dkdrakekendall@gmail.com>
2026-03-04 21:53:00 -0700

    added projects backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,35 @@
 from django.db import models
+import uuid
+from django.utils.text import slugify
 
+# Was an example, but now TODO
 class Task(models.Model):
     name = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    # Note: slug isn't unique so not used for actual lookups, just for nicer urls. pk is still used for lookups
+    slug = models.SlugField()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    # TODO stuff involving users and auth
+    created_by = models.CharField(max_length=100)
+    modified_by = models.CharField(max_length=100)
+    #soft delete feature possibly to be implemented
+    is_deleted = models.BooleanField(default=False)
+
+    # making slug based off name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
+
+
+
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import TaskListCreateAPIView
+from .views import TaskListView, ProjectListView, ProjectView
 
 urlpatterns = [
-    path("tasks/", TaskListCreateAPIView.as_view(), name="tasks"),
+    path("tasks/", TaskListView.as_view(), name="tasks"),
+
+    # api/projects/ - GET, POST
+    path("projects/", ProjectListView.as_view(), name="projects"),
+
+    # api/projects/<int:pk>/ - GET, PUT, DELETE
+    path("projects/<int:pk>/", ProjectView.as_view(), name="project-detail"),
+
+    # api/projects/<int:pk>/<slug:slug>/ - GET, PUT, DELETE
+    # still uses pk to find the project, slug just makes url look nicer
+    path("projects/<int:pk>/<slug:slug>/", ProjectView.as_view(), name="project-detail-slug"),
 ]
